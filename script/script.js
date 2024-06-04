0888ede4af6f25ac49d63fb832ccb82c446cb00a
kingkrazee <kaydenhendrick04@gmail.com>
2024-06-04 16:23:12 +0200

    nextnumb

---
+++ script/script.js
@@ -0,0 +1,22 @@
+// let currentNumber = 9;
+// let nextNumber = currentNumber + 1;
+// console.log("Next number:", nextNumber);
+let k=0
+let kayden =[20,2,6,8]
+function thankYouNext() {
+    if (k<kayden.length) {
+       console.log(kayden[t]);
+    }
+    t++
+}
+thankYouNext()
+thankYouNext()
+thankYouNext()
+
+
+
+
+
+
+
+
